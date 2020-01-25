8556ed628e81c81f69e60cc2c3321ecf37bad92b
Parker Johnson <mrstormthebeach@gmail.com>
2020-01-25 09:57:09 -0700

    clean up unused code in project

--- src/strongapp/database.clj
+++ src/strongapp/database.clj
@@ -4,11 +4,6 @@
    [clojure.set :as cljset]))
 
 
-(defn sqlQuery
-  []
-  (jdbc/query "postgresql://strongapp:foobar@localhost:5432/strongdb"
-             ["SELECT * from test"]))
-
 (defn insert
   [spec data]
   (jdbc/insert-multi! spec :test_exercises data))
@@ -20,17 +15,8 @@
                    :let [y (get d :date)]]
                y))
   (def datemap (map #(hash-map :id %2 :date %1) (set dates) (iterate inc 0)) )
-  ;; don't use, not idiomatic (clojure.set/join datemap data)
   (def newdata (reduce (fn [acc m] (assoc acc (:date m) (:id m))) {} datemap))
   (def moredata (into #{} (map #(assoc % :id (newdata (:date %)))) data))
 
-  (for [d moredata] (cljset/rename-keys d {:id :exercise-id}))
-  )
-
-;;(defn pooled-db
-;;  []
-;;  (pg/pool :host "localhost" :user "strongapp" :dbname "strongdb" :password "foobar"))
-
-;;(jdbc/query pooled-db ["SELECT '* from test'"])
-
-;;(pg/close! pooled-db)
+  ;; TODO - Do this earlier
+  (for [d moredata] (cljset/rename-keys d {:id :exercise-id})))
